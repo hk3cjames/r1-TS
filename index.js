c41ad54b6704ee1bdb60c422af41df133edf6271
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-09-19 20:55:49 +0800

    modify rx from PS

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // input hash from ts to PS
 const express = require("express");
 const app = express();
+const fetch = require("node-fetch");
+
 app.listen(3000, () =>
   console.log("PS-1001 listening at localhost(or https):3000")
 );
@@ -8,36 +10,37 @@ app.use(express.static("ts"));
 
 app.use(express.json({ limit: "1mb" }));
 
+var res1 = {};
 existHash = "ab1234";
-app.post("/hashFile", (req, res) => {
+app.post("/hashFile", async (req, res) => {
   rxjson = req.body;
   console.log(rxjson);
 
   time = Date.now();
-  newHash = rxjson.thash
-  console.log(newHash)
-  console.log(existHash)
+  newHash = rxjson.thash;
+  console.log(newHash);
+  console.log(existHash);
   if (newHash === existHash) {
     res_status = "fail";
   } else {
     res_status = "success";
     existHash = rxjson.thash;
-
   }
   res.json({
     status: res_status,
     tick: time,
   });
 
-  // data = rxjson
-  // const options = {
-  //   method: "POST",
-
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(data),
-  // };
-  // const res1 = fetch("http://192.168.0.110:3100/hashFile", options);
+  data = rxjson;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  res1 = await fetch("http://192.168.0.110:3100/hashFile", options)
+  const disp = await res1.json();
+  console.log(disp)
 
 });
